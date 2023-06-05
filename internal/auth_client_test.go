284abd62ee747d15394df5a9e8e899f965231e5a
Shane Hender <henders@users.noreply.github.com>
2023-06-05 13:49:43 -0700

    simplify tests

--- internal/auth_client_test.go
+++ internal/auth_client_test.go
@@ -8,43 +8,40 @@ import (
 )
 
 func TestAuthClient_RequestJWT(t *testing.T) {
-	vmContext := InitPlugin(t)
-	t.Run("call auth service with authorization header", func(t *testing.T) {
-		// Initialize new plugin
-		host, contextID, reset := NewContext(t, vmContext)
-		defer reset()
+	// Initialize new plugin with http context
+	host, contextID, reset := NewContext(t, InitPlugin(t))
+	defer reset()
 
-		// Call OnRequestHeaders to initialize context
-		_ = host.CallOnRequestHeaders(contextID, [][2]string{
-			{AuthHeader, "MAC ts....."},
-		}, true)
+	// Call OnRequestHeaders to initialize context
+	_ = host.CallOnRequestHeaders(contextID, [][2]string{{AuthHeader, "MAC ts....."}}, true)
 
-		// Verify Auth Service is called with DispatchHttpCall
-		require.Equal(t, 1, len(host.GetCalloutAttributesFromContext(contextID)))
-		// At this point, none of dispatched callouts received response therefore the current status must be paused.
-		require.Equal(t, types.ActionPause, host.GetCurrentHttpStreamAction(contextID))
+	// Verify Auth Service is called with DispatchHttpCall by checking the length of the callout array
+	require.Equal(t, 1, len(host.GetCalloutAttributesFromContext(contextID)))
 
-		// Get the handle to the DispatchHttpCall request
-		callout := host.GetCalloutAttributesFromContext(contextID)[0]
+	// At this point, none of dispatched callouts received response therefore the current status must be paused.
+	require.Equal(t, types.ActionPause, host.GetCurrentHttpStreamAction(contextID))
 
-		// Validate we sent the Auth Service the right headers
-		require.Equal(t, [][2]string{
-			{"accept", "*/*"},
-			{":authority", "auth"},
-			{":method", "GET"},
-			{":path", "/base64/RkFLRV9KV1QK"},
-			{AuthHeader, "MAC ts....."},
-		}, callout.Headers)
+	// Get the handle to the DispatchHttpCall request
+	callout := host.GetCalloutAttributesFromContext(contextID)[0]
 
-		// Now have Auth Service respond to handler
-		host.CallOnHttpCallResponse(callout.CalloutID, [][2]string{{":status", "200"}}, nil, []byte("test JWT"))
+	// Validate we sent the Auth Service the right headers
+	require.Equal(t, [][2]string{
+		{"accept", "*/*"},
+		{":authority", "auth"},
+		{":method", "GET"},
+		{":path", "/base64/RkFLRV9KV1QK"},
+		{AuthHeader, "MAC ts....."},
+	}, callout.Headers)
 
-		// Verify the JWT from the above request was added to the original request's headers
-		require.Equal(t, [][2]string{
-			{AuthHeader, "MAC ts....."},
-			{"x-auth-jwt", "test JWT"},
-		}, host.GetCurrentRequestHeaders(contextID))
-		// The request should have been marked as continued now after processing Auth response
-		require.Equal(t, types.ActionContinue, host.GetCurrentHttpStreamAction(contextID))
-	})
+	// Now have a pretend Auth Service respond to our callback handler. Note we can specify any response headers or bodies.
+	host.CallOnHttpCallResponse(callout.CalloutID, [][2]string{{":status", "200"}}, nil, []byte("test JWT"))
+
+	// Verify the JWT from the above request was added to the original request's headers
+	require.Equal(t, [][2]string{
+		{AuthHeader, "MAC ts....."},
+		{"x-auth-jwt", "test JWT"},
+	}, host.GetCurrentRequestHeaders(contextID))
+
+	// The request should now have been marked as continued after processing Auth response
+	require.Equal(t, types.ActionContinue, host.GetCurrentHttpStreamAction(contextID))
 }
